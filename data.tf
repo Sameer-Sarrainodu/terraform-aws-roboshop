0632e22b885d72e91e7f730781abc3ee9a6c2b6e
b27user12 <b27user12@rpsconsulting.in>
2025-08-25 13:16:25 +0530

    yup

--- data.tf
+++ data.tf
@@ -6,7 +6,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 }
 
 
-data "aws_ssm_parameter" "catalogue_sg_id" {
+data "aws_ssm_parameter" "sg_id" {
   name  = "/${var.project}-${var.environment}/${var.component}_sg_id"
 }
 
